f575ddb26896b2868f08772e5b9c994345ff458c
Feng Li <fengli@smartx.com>
2017-05-09 15:05:40 +0800

    push to host

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+host=${HOST:-192.168.31.188}
 perf record -F 99 -p $2 -g -- sleep $1
 perf script | ./tools/stackcollapse-perf.pl > $3.perf-folded
 ./tools/flamegraph.pl $3.perf-folded > $3.svg
 rm $3.perf-folded
+echo "Push to host: $host"
+scp $3.svg root@"$host":/var/www/html/
